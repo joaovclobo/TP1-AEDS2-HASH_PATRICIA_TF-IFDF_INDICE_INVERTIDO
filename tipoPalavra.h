c6391c2ce9c87f6546b4ec5ceeb8d9481f3c422c
João Lobo <91331548+joaovclobo@users.noreply.github.com>
2022-06-13 11:11:18 -0300

    Definição das funções

--- tipoPalavra.h
+++ tipoPalavra.h
@@ -1,9 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "listaEncadPares.h"
 #define tamMaxPalavra 50
 
 typedef struct tipoPalavra{
-    char palavra[tamMaxPalavra];
-    
+    char valPalavra[tamMaxPalavra];
+    listaEncadPares* listaPares;
 }tipoPalavra;
+
+/*---------------- Gets e sets ---------------- */
+
+char* getPalavra(tipoPalavra* palavra);
+
+tipoPar* getPar(tipoPalavra* palavra, int idDoc); //Busca e retorna o valor de um par da lista com base em um idDoc
+
+listaEncadPares* getListaPares(tipoPalavra* palavra);
+
+void setPalavra(tipoPalavra* palavra, char* valPalavra); 
+
+void setPar(tipoPalavra* palavra, tipoPar par);
+
+void setListaPares(tipoPalavra* palavra, listaEncadPares* listapares);
+
+/*---------------- Funções auxiliares ---------------- */
+
+void inicialzaPalavra(tipoPalavra* palavra, char* valPalavra, tipoPar primPar); //Inicia um tipoPalavra já com o valor da palavra e um par(qnte, idDoc)
+
+void aumentaQtdePar(tipoPalavra* palavra, int idDoc);  //Aumenta em 1 o valor de repetições de uma palavra do documento
+
+int comparaPalavra(tipoPalavra palavra1, tipoPalavra palavra2); //Retorna 1 se forem iguais e 0 se diferentes
