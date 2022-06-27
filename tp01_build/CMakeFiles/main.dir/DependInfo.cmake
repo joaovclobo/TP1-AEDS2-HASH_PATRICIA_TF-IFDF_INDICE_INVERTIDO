d970b2ebd9904b065103558e1de5bb8e03367e40
Guilherme-Schwann <guilhermeawilke@hotmail.com>
2022-06-27 19:57:35 -0300

    cmakelists feito

---
+++ tp01_build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/guto7k/Documents/UFV-CDC/2022.1/AEDS2/tp01-AEDS2/hashTablePalavras.c" "CMakeFiles/main.dir/hashTablePalavras.c.o" "gcc" "CMakeFiles/main.dir/hashTablePalavras.c.o.d"
+  "/home/guto7k/Documents/UFV-CDC/2022.1/AEDS2/tp01-AEDS2/listaEncadDocs.c" "CMakeFiles/main.dir/listaEncadDocs.c.o" "gcc" "CMakeFiles/main.dir/listaEncadDocs.c.o.d"
+  "/home/guto7k/Documents/UFV-CDC/2022.1/AEDS2/tp01-AEDS2/listaEncadPares.c" "CMakeFiles/main.dir/listaEncadPares.c.o" "gcc" "CMakeFiles/main.dir/listaEncadPares.c.o.d"
+  "/home/guto7k/Documents/UFV-CDC/2022.1/AEDS2/tp01-AEDS2/main.c" "CMakeFiles/main.dir/main.c.o" "gcc" "CMakeFiles/main.dir/main.c.o.d"
+  "/home/guto7k/Documents/UFV-CDC/2022.1/AEDS2/tp01-AEDS2/operacoesIndiceInvertido.c" "CMakeFiles/main.dir/operacoesIndiceInvertido.c.o" "gcc" "CMakeFiles/main.dir/operacoesIndiceInvertido.c.o.d"
+  "/home/guto7k/Documents/UFV-CDC/2022.1/AEDS2/tp01-AEDS2/patricia.c" "CMakeFiles/main.dir/patricia.c.o" "gcc" "CMakeFiles/main.dir/patricia.c.o.d"
+  "/home/guto7k/Documents/UFV-CDC/2022.1/AEDS2/tp01-AEDS2/tipoPalavra.c" "CMakeFiles/main.dir/tipoPalavra.c.o" "gcc" "CMakeFiles/main.dir/tipoPalavra.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tp01_build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/hashTablePalavras.c.o"
+  "CMakeFiles/main.dir/hashTablePalavras.c.o.d"
+  "CMakeFiles/main.dir/listaEncadDocs.c.o"
+  "CMakeFiles/main.dir/listaEncadDocs.c.o.d"
+  "CMakeFiles/main.dir/listaEncadPares.c.o"
+  "CMakeFiles/main.dir/listaEncadPares.c.o.d"
+  "CMakeFiles/main.dir/main.c.o"
+  "CMakeFiles/main.dir/main.c.o.d"
+  "CMakeFiles/main.dir/operacoesIndiceInvertido.c.o"
+  "CMakeFiles/main.dir/operacoesIndiceInvertido.c.o.d"
+  "CMakeFiles/main.dir/patricia.c.o"
+  "CMakeFiles/main.dir/patricia.c.o.d"
+  "CMakeFiles/main.dir/tipoPalavra.c.o"
+  "CMakeFiles/main.dir/tipoPalavra.c.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
