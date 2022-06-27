a533c144c9b02d504ab819640a57c78485180391
Guilherme-Schwann <guilhermeawilke@hotmail.com>
2022-06-27 14:52:43 -0300

    att patricia.c

--- patricia.c
+++ patricia.c
@@ -2,7 +2,7 @@
 
 char letra(tipoPalavra k, int i){
     if (strlen(k.valPalavra) < i)
-        return "0";
+        return ' ';
     return k.valPalavra[i-1];
 }
 
@@ -11,13 +11,19 @@ short eExterno(tipoArvore p){
     return (p->nt == externo);
 }
 
-tipoArvore CriaNoInt(int i, tipoArvore *esq,  tipoArvore *dir){
-    tipoArvore p;
-    p = (tipoArvore)malloc(sizeof(tipoNo));
+tipoArvore criaNoInt(int i, tipoArvore *esq,  tipoArvore *dir){
+    tipoArvore p = (tipoArvore)malloc(sizeof(tipoNo));
     p->nt = interno;
     p->no.nInterno.esq = *esq;
     p->no.nInterno.dir = *dir;
     p->no.nInterno.index = i;
+    tipoArvore aux = (tipoArvore)malloc(sizeof(tipoNo));
+    aux = p;
+    while (!eExterno(aux)){
+        aux = aux->no.nInterno.dir;
+    }
+    p->no.nInterno.indexLetra = letra(aux->no.palavra, i);
+    printf("Debug no int esq %s | dir %s | index %d %c\n", (*esq)->no.palavra.valPalavra, (*dir)->no.palavra.valPalavra, i, p->no.nInterno.indexLetra);
     return p;
 } 
 
@@ -35,13 +41,13 @@ tipoArvore insereEntre(tipoPalavra k, tipoArvore *t, int i)
   if (eExterno(*t) || i < (*t)->no.nInterno.index){
     /* cria um novo no externo */
     p = criaNoExt(k);
-    if (letra(k, i) != (*t)->no.nInterno.indexLetra) 
+    if (letra(k, i) == (*t)->no.nInterno.indexLetra) 
         return (criaNoInt(i, t, &p));
     else
         return (criaNoInt(i, &p, t));
   } 
   else{
-    if (letra(k, (*t)->no.nInterno.index) != (*t)->no.nInterno.indexLetra)
+    if (letra(k, (*t)->no.nInterno.index) == (*t)->no.nInterno.indexLetra)
         (*t)->no.nInterno.dir = insereEntre(k,&(*t)->no.nInterno.dir,i);
     else
         (*t)->no.nInterno.esq = insereEntre(k,&(*t)->no.nInterno.esq,i);
@@ -49,53 +55,66 @@ tipoArvore insereEntre(tipoPalavra k, tipoArvore *t, int i)
   }
 }
 
-tipoArvore inserePatricia(char* valPalavra, int idDoc, tipoArvore *t){
-    tipoPalavra palavra;
-    inicializaPalavra(&palavra, valPalavra, idDoc);
+tipoArvore insere(char* valPalavra, int idDoc, tipoArvore *t){
+    tipoPalavra* palavra = (tipoPalavra*) malloc(sizeof(tipoPalavra));
+    inicializaPalavra(palavra, valPalavra, idDoc);
     tipoArvore p;
     int i;
     if (*t == NULL) 
-        return (criaNoExt(palavra));
+        return (criaNoExt(*palavra));
     else {
         p = *t;
         while (!eExterno(p)){
-            if (letra(palavra, p->no.nInterno.index) == p->no.nInterno.indexLetra)
+            if (letra(*palavra, p->no.nInterno.index) != p->no.nInterno.indexLetra)
                 p = p->no.nInterno.esq;
             else
                 p = p->no.nInterno.dir;
         }
         /* acha o primeiro bit diferente */
         i = 1;
-        while ((i <= tamMaxPalavra) & (letra(palavra, i) == letra(p->no.palavra, i))) 
+        while ((i <= tamMaxPalavra) && (letra(*palavra, i) == letra(p->no.palavra, i))) 
             i++;
         if (i > tamMaxPalavra){
-            aumentaQtdePar(&(*t)->no.palavra, idDoc);
+            if(buscaIdDoc(*palavra->listaPares, idDoc))
+                aumentaQtdePar(&(*t)->no.palavra, idDoc);
+            else
+                insereNovoIdDoc(&(*t)->no.palavra, idDoc);
             return (*t);
         } 
         else
-            return (insereEntre(palavra, t, i));
+            return (insereEntre(*palavra, t, i));
     }
 }
 
 
-tipoPalavra pesquisaPatricia(char* palavra, tipoArvore t){
+tipoPalavra pesquisa(char palavra[50], tipoArvore t){
     if (eExterno(t)){
-        if (palavra == t->no.palavra.valPalavra) 
+        printf("%s", t->no.palavra.valPalavra);
+        if (strcmp(palavra, t->no.palavra.valPalavra) == 0)
             return t->no.palavra;
         else printf("Elemento nao encontrado\n");
-    return;
+        tipoPalavra* aux = (tipoPalavra*) malloc(sizeof(tipoPalavra));
+        inicializaPalavra(aux, t->no.palavra.valPalavra, 0);
+        return *aux;
     }
-    if (palavra[t->no.nInterno.index] == t->no.nInterno.indexLetra) 
-        pesquisaPatricia(palavra, t->no.nInterno.esq);
-    else
-        pesquisaPatricia(palavra, t->no.nInterno.dir);
+    tipoPalavra* aux = (tipoPalavra*) malloc(sizeof(tipoPalavra));
+    inicializaPalavra(aux, palavra, 0);
+    if (letra(*aux, t->no.nInterno.index) == t->no.nInterno.indexLetra){
+        printf("nó interno: %c\n", t->no.nInterno.indexLetra);
+        pesquisa(palavra, t->no.nInterno.dir);
+    }
+    else{
+        printf("nó interno: %c\n", t->no.nInterno.indexLetra);
+        pesquisa(palavra, t->no.nInterno.esq);
+    }
 }
 
-int quantasPalavrasPatricia(tipoArvore t, int idDoc){
-    int num;
-    if (eExterno(t))
-        return buscaIdDoc(*t->no.palavra.listaPares, idDoc);
-    num += quantasPalavrasPatricia(t->no.nInterno.esq, idDoc);
-    num += quantasPalavrasPatricia(t->no.nInterno.dir, idDoc);
+int quantasPalavras(tipoArvore t, int idDoc){
+    int num = 0;
+    if (eExterno(t)){
+        return buscaIdDocQuant(*t->no.palavra.listaPares, idDoc);
+    }
+    num += quantasPalavras(t->no.nInterno.esq, idDoc);
+    num += quantasPalavras(t->no.nInterno.dir, idDoc);
     return num;
 }
